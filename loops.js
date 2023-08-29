98f240f5e0202923a13c3f1f485dd705987b229b
Denys Pavlyuk <densf2@gmail.com>
2023-08-29 20:44:55 +0300

    added_while_loop

--- loops.js
+++ loops.js
@@ -1,2 +1,16 @@
 // the initial changes for work with JS
 // author Den
+
+let num = 10
+
+// while (num <= 7) {
+//     console.log(num)
+//     num++
+// }
+
+
+//do while
+// do {
+//     num--
+//     console.log(num)
+// } while (num <= 7)
