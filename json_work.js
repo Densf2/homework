578ebc219e97af2fb7fccec8b03b1d91a28f8a27
Denys Pavlyuk <densf2@gmail.com>
2023-09-05 20:58:08 +0300

    added_rest_changes

--- json_work.js
+++ json_work.js
@@ -1,4 +1,36 @@
-const values = require('./values.json')
+// const values = require('./values.json')
+// const fs = require('fs-extra')
+// const { writeFile, readFile } = require('fs-extra');
+
+// const path = './values.json'
+
+// readFile(path, (error, data) => {
+//     if (error) {
+//       console.log(error);
+//       return;
+//     }
+//     const parsedData = JSON.parse(data);
+//     parsedData.createdAt = new Date().toISOString();
+//     writeFile(path, JSON.stringify(parsedData, null, 2), (err) => {
+//       if (err) {
+//         console.log('Failed to write updated data to file');
+//         return;
+//       }
+//       console.log('Updated file successfully');
+//     });
+//   });
+  
+
+// function Func() {
+//     fetch("./values.json")
+//         .then((res) => {
+//         return res.json();
+//     })
+//     .then((data) => console.log(data));
+// }
+// Func()
+
+// console.log(readData)
 
 // let jsonObj1 = '{"name":"den","lname":"pavliuk","age":100500}'
 
@@ -12,4 +44,48 @@ const values = require('./values.json')
 
 // jsonObj1 = JSON.stringify(obj2)
 
-console.log(values)
+// console.log(values)
+
+
+// let path = "./values6.json";
+// let data, jsonData;
+// try {
+//     if(fs.existsSync(path)) {
+//         data = fs.readFileSync(path, 'utf8');
+//         if(data) {
+//             jsonData = JSON.parse(data);
+//             if(typeof jsonData === 'object' && jsonData !== null && !Array.isArray(jsonData)) {
+//                 jsonData.test = 'test';
+//                 jsonData.email = 'test@gmail.com';
+//                 fs.writeFileSync(path, JSON.stringify(jsonData));
+//             } else {
+//                 throw new Error("Parsed JSON data is not an object");
+//             }
+//         } else {
+//             throw new Error("No data read from the file");
+//         }
+//     } else {
+//         throw new Error("File does not exist");
+//     }
+// } catch (err) {
+//     console.error(err.message);
+// }
+
+// let ddd = JSON.parse(fs.readFileSync(path, 'utf-8'))
+// console.log(ddd)
+
+// ddd.newVAl = 'Myval'
+
+// fs.writeFileSync(path, JSON.stringify(ddd))
+const fs = require('fs-extra');
+const path = './values.json';
+
+async function func1() {
+    let ddd = await JSON.parse(fs.readFileSync(path, 'utf-8'))
+    console.log(ddd)
+    console.log('Hello from async function')
+    ddd.dkd = 'DKD'
+    await fs.writeFile(path, JSON.stringify(ddd))
+}
+
+func1()
