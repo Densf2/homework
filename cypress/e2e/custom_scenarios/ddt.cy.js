5fe4578f15c011cfbb803e1b8a4108b289142f12
Denys Pavlyuk <densf2@gmail.com>
2023-10-06 22:21:29 +0300

    added_ddt_example

---
+++ cypress/e2e/custom_scenarios/ddt.cy.js
@@ -0,0 +1,16 @@
+
+import { users } from "../../data/search_text.js";
+
+// example of parametrized test
+for (const {id, searchVal} of users) {
+    it(`${id} check search`, () => {
+        cy.visit('/')
+        cy.get('input#txtGlobalSearch').click()
+        .type(`${searchVal}{enter}`)
+        cy.url().should('contain', searchVal)
+    })
+}
+
+
+
+
