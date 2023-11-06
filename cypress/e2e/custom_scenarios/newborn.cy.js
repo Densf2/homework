a7f1f28588f5096736995aa2f2adc5e93a6f01ff
Denys Pavlyuk <densf2@gmail.com>
2023-11-06 21:09:58 +0200

    added_faker_package

--- cypress/e2e/custom_scenarios/newborn.cy.js
+++ cypress/e2e/custom_scenarios/newborn.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
-
+import { faker } from "@faker-js/faker"
+//http://5.189.186.217
+//ENV_URL=http://5.189.186.217 npm run cy:run_chrome -- --spec cypress/e2e/custom_scenarios/query_mongo.cy.js
 
 describe('login with api request', () => {
 
@@ -13,10 +15,28 @@ describe('login with api request', () => {
     })
 
     it('Create category', () => {
-        cy.createNewCategory('soap')
+        let categoryName = faker.commerce.product()
+        cy.createNewCategory(categoryName)
+        cy.task('log', categoryName)
         cy.visit('/categories')
         cy.wait(5000)
         cy.get('.content a.collection-item', {timeout: 5000}).should('be.visible')
+    })
+
+    it('create product for category', () => {
+        let productName = faker.commerce.productName()
+        cy.createProduct(productName)
+        cy.task('log', productName)
+        cy.visit(`/categories/${Cypress.env('categoryId')}`)
+        cy.get('a.collection-item span').eq(0)
+        .should('be.visible')
+        .contains(productName)
+    })
+
+    it('remove the category by id', () => {
+        cy.removeCategoryById()
+        cy.visit(`/categories/${Cypress.env('categoryId')}`)
+        .should('not.be.visible')
     })
 
 })
