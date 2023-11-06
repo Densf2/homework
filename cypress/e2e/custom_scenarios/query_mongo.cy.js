54e5d803d1a074ec963c8983421602073c68a076
Denys Pavlyuk <densf2@gmail.com>
2023-11-06 21:11:41 +0200

    added_token_into_variable

--- cypress/e2e/custom_scenarios/query_mongo.cy.js
+++ cypress/e2e/custom_scenarios/query_mongo.cy.js
@@ -1,5 +1,8 @@
 import { ObjectId } from 'mongodb'
 
+//list of commands
+// docker run --name mongo -p 27017:27017 -d mongodb/mongodb-community-server:6.0-ubuntu2204
+//docker exec -it mongo mongosh
 
 describe('Find data', () => {
 
@@ -11,6 +14,40 @@ describe('Find data', () => {
         })
     })
 
-    
+    context('UI & API request', () => {
 
+        it('login by UI', () => {
+            cy.request({
+                method: 'POST',
+                url: '/api/users/authenticate',
+                body: {username:"dpdp1",password:"abc123"},
+            }).then((response) => {
+            const authToken = response.body.token
+            Cypress.env('authTNext13', authToken)
+            })
+        })
+
+        it('create user', () => {
+            cy.request({
+                method: 'POST',
+                url: '/api/users/register',
+                headers: {
+                    Authorization: `Bearer ${Cypress.env('authTNext13')}`
+                },
+                body: {
+                    firstName: 'fff2',
+                    lastName: 'sss2',
+                    username: 'fffsss2',
+                    password: 'abc1232',
+                }
+            })
+        })
+
+        it('findMany', () => {
+            cy.findMany({}, { collection: 'users' }).then((mongoresult) => {
+                cy.task('log', mongoresult)
+            })
+        })
+
+    })
 })
