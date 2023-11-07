ee0285a782aa456661144afa7e0b8e6b83b26c6f
Denys Pavlyuk <densf2@gmail.com>
2023-11-07 21:17:08 +0200

    updated_file_due_to_rules

--- cypress/e2e/custom_scenarios/newborn.cy.js
+++ cypress/e2e/custom_scenarios/newborn.cy.js
@@ -1,42 +1,42 @@
 /// <reference types="cypress" />
 import { faker } from "@faker-js/faker"
-//http://5.189.186.217
-//ENV_URL=http://5.189.186.217 npm run cy:run_chrome -- --spec cypress/e2e/custom_scenarios/query_mongo.cy.js
+// http://5.189.186.217
+// ENV_URL=http://5.189.186.217 npm run cy:run_chrome -- --spec cypress/e2e/custom_scenarios/query_mongo.cy.js
 
 describe('login with api request', () => {
+  beforeEach(() => {
+    cy.loginAndSetLocalStorage()
+  })
 
-    beforeEach(() => {
-        cy.loginAndSetLocalStorage()
-    })
+  it('send login data', () => {
+    cy.visit('/overview')
+    cy.get('div.card-content').eq(0).should('be.visible')
+  })
 
-    it('send login data', () => {
-        cy.visit('/overview')
-        cy.get('div.card-content').eq(0).should('be.visible')
-    })
+  it('Create category', () => {
+    let categoryName = faker.commerce.product()
+    cy.createNewCategory(categoryName)
+    cy.task('log', categoryName)
+    cy.visit('/categories')
+    cy.wait(5000)
+    cy.get('content a.collection-item', { timeout: 5000 }).should('be.visible')
+  })
 
-    it('Create category', () => {
-        let categoryName = faker.commerce.product()
-        cy.createNewCategory(categoryName)
-        cy.task('log', categoryName)
-        cy.visit('/categories')
-        cy.wait(5000)
-        cy.get('content a.collection-item', {timeout: 5000}).should('be.visible')
-    })
+  it('create product for category', () => {
+    let productName = faker.commerce.productName()
+    cy.createProduct(productName)
+    cy.task('log', productName)
+    cy.visit(`/categories/${Cypress.env('categoryId')}`)
+    cy.get('a.collection-item span').eq(0)
+      .should('be.visible')
+      .contains(productName)
+  })
 
-    it('create product for category', () => {
-        let productName = faker.commerce.productName()
-        cy.createProduct(productName)
-        cy.task('log', productName)
-        cy.visit(`/categories/${Cypress.env('categoryId')}`)
-        cy.get('a.collection-item span').eq(0)
-        .should('be.visible')
-        .contains(productName)
-    })
+  // comment for testing
 
-    it('remove the category by id', () => {
-        cy.removeCategoryById()
-        cy.visit(`/categories/${Cypress.env('categoryId')}`)
-        .should('not.be.visible')
-    })
-
+  it('remove the category by id', () => {
+    cy.removeCategoryById()
+    cy.visit(`/categories/${Cypress.env('categoryId')}`)
+      .should('not.be.visible')
+  })
 })
